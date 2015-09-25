3e9892f6b5cbad907b397aba6d48dd6bbf35843d
Michael Grube <michael.grube@gmail.com>
2015-09-25 09:05:44 -0400

    added JDK, scala, spark

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,8 @@ apt-get -y install mysql-server
 apt-get -y install python-pip
 apt-get -y install python-mysqldb
 apt-get -y install git
+apt-get -y install default-jre
+apt-get -y install default-jdk
 git clone https://github.com/alanmcintyre/btce-api.git
 cd btce-api
 python setup.py install
@@ -20,3 +22,11 @@ echo "*/2 * * * * python /vagrant/fetch_btce.py ltc_usd" >> crontasks
 echo "*/2 * * * * python /vagrant/fetch_btce.py ppc_usd" >> crontasks
 echo "*/2 * * * * python /vagrant/fetch_btce.py ppc_btc" >> crontasks
 crontab crontasks
+wget wget http://downloads.typesafe.com/scala/2.11.7/scala-2.11.7.tgz?_ga=1.262311372.1507431377.1443137757
+mv scala-2.11.7.tgz?_ga=1.262311372.1507431377.1443137757 ./scala.tgz
+tar -xzf ./scala.tgz
+cd ./scala-2.11.7
+cp -rf ./bin/* /bin/
+cp -rf ./lib/* /lib/
+wget http://www.eu.apache.org/dist/spark/spark-1.5.0/spark-1.5.0-bin-hadoop2.6.tgz
+tar -xzf ./spark-1.5.0-bin-hadoop2.6.tgz
