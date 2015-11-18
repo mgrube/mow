b417f0814cac298e35bd1972abf5f032efb2fa6d
Michael Grube <michael.grube@gmail.com>
2015-11-18 16:09:03 -0500

    cleanup

--- btcmonitor.py
+++ btcmonitor.py
@@ -1,4 +1,3 @@
-#This is a modified version of the network wordcount example.
 #Exercise: Do something useful with the output
 
 from __future__ import print_function
@@ -9,6 +8,8 @@ from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 import datetime
 
+#Normally in Spark you'd use a Window. 
+#We cannot do this with our stream because it simply returns the last 150 trades
 def isWithin30Sec(time):
 	if datetime.datetime.strptime(time, '%Y-%m-%d %H:%M:%S') >= datetime.datetime.now() - datetime.timedelta(0, 30):
 		return True
@@ -19,11 +20,12 @@ if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Usage: btcmonitor.py <hostname> <port>", file=sys.stderr)
         exit(-1)
+
     sc = SparkContext(appName="BTCPriceMonitor")
     ssc = StreamingContext(sc, 1)
 
     lines = ssc.socketTextStream(sys.argv[1], int(sys.argv[2]))
-    #lines.pprint(250)
+   
     prices = lines.filter(lambda line: len(line.split(',')) > 5)\
 		  .filter(lambda line: isWithin30Sec(line.split(',')[5]))\
             
@@ -32,8 +34,8 @@ if __name__ == "__main__":
     sums = sums.join(counts)
     avg = sums.map(lambda k: (k[0], k[1][0]/k[1][1]))
 
-    #agg = prices.reduce(lambda a, b : (a + b)).map(lambda a: a/count)
+
     avg.pprint()
-    #prices.pprint()
+ 
     ssc.start()
     ssc.awaitTermination()
