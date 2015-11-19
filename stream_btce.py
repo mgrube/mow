5f33ceb0c49b58ad386a9f9b42aca5276469e364
Michael Grube <michael.grube@gmail.com>
2015-11-19 10:53:34 -0500

    every 30 seconds not every 5

--- stream_btce.py
+++ stream_btce.py
@@ -6,7 +6,7 @@ import sys
 import time
 
 while True:
-	time.sleep(5)
+	time.sleep(30)
 	alltrades = btceapi.getTradeHistory(sys.argv[1])
 	for t in alltrades:
 		print str(t.pair) + ',' +  str(t.price) + ',' +  str(t.amount) + ',' +  str(t.tid) + ',' + str(t.trade_type) + ',' + str(t.date)
